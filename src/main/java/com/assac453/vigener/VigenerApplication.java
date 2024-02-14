3c2a2344b8b7a0ffd41a27fd88011889238a5954
assac453 <ritterfrosch@yandex.ru>
2024-02-14 12:10:47 +0300

    init

---
+++ src/main/java/com/assac453/vigener/VigenerApplication.java
@@ -0,0 +1,13 @@
+package com.assac453.vigener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assac453/vigener/VigenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assac453.vigener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
