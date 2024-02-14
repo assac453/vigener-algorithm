e49c1a53236dc265cac1313e24520533b29f63b9
assac453 <ritterfrosch@yandex.ru>
2024-02-14 21:03:06 +0300

    feat: algoritm vigener

---
+++ src/main/java/com/assac453/vigener/algorithm/VigenerAlgorithmImpl.java
@@ -0,0 +1,42 @@
+package com.assac453.vigener.algorithm;
+
+
+public class VigenerAlgorithmImpl implements VigenerAlgorithm{
+    final private int bias;
+    final private int letters;
+    public VigenerAlgorithmImpl(int bias, int letters) {
+        this.bias = bias;
+        this.letters = letters;
+    }
+
+    public String encrypt(final String text, final String key){
+        StringBuilder resultString = new StringBuilder();
+        StringBuilder encrypt = new StringBuilder();
+        for (String phrase: text.split(" ")) {
+            final int keyLen = key.length();
+            for (int i = 0, len = phrase.length(); i < len; i++) {
+                encrypt.append((char) (((phrase.charAt(i) + key.charAt(i % keyLen) - 2 * this.bias) % this.letters) + this.bias));
+            }
+            resultString.append(encrypt.toString());
+            resultString.append(" ");
+            encrypt.setLength(0);
+        }
+        return resultString.toString();
+    }
+
+    public String decrypt(final String cipher, final String key) {
+        StringBuilder resultString = new StringBuilder();
+        StringBuilder decrypt = new StringBuilder();
+        for (String phrase: cipher.split(" ")) {
+            final int keyLen = key.length();
+            for (int i = 0, len = phrase.length(); i < len; i++) {
+                decrypt.append((char) (((phrase.charAt(i) - key.charAt(i % keyLen) + this.letters) % this.letters) + this.bias));
+            }
+            resultString.append(decrypt.toString());
+            resultString.append(" ");
+            decrypt.setLength(0);
+        }
+        return resultString.toString();
+    }
+
+}
