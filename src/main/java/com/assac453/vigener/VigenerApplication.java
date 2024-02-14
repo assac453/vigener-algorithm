d42955458731ba5d4df3e225e82764d6df81b0a8
assac453 <ritterfrosch@yandex.ru>
2024-02-14 21:01:40 +0300

    feat: add vigener algorithm

--- src/main/java/com/assac453/vigener/VigenerApplication.java
+++ src/main/java/com/assac453/vigener/VigenerApplication.java
@@ -1,13 +1,24 @@
 package com.assac453.vigener;
 
+import com.assac453.vigener.file.FileManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VigenerApplication {
+@RequiredArgsConstructor
+public class VigenerApplication implements CommandLineRunner {
 
+	private final FileManager fileManager;
 	public static void main(String[] args) {
 		SpringApplication.run(VigenerApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		fileManager.encrypt();
+		fileManager.decrypt();
+	}
 }
