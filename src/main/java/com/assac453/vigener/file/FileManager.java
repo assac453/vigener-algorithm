d8e1a6a561ff28a6c08481a5b68daa3df0fb965b
assac453 <ritterfrosch@yandex.ru>
2024-02-14 22:07:18 +0300

    refactor: add file path providers as beans

--- src/main/java/com/assac453/vigener/file/FileManager.java
+++ src/main/java/com/assac453/vigener/file/FileManager.java
@@ -1,6 +1,11 @@
 package com.assac453.vigener.file;
 
+import com.assac453.vigener.algorithm.FrequencyAnalysis;
 import com.assac453.vigener.algorithm.VigenerAlgorithm;
+import com.assac453.vigener.file.pathprovider.AnalyzedFilePathProvider;
+import com.assac453.vigener.file.pathprovider.DecryptedFilePathProvider;
+import com.assac453.vigener.file.pathprovider.EncryptedFilePathProvider;
+import com.assac453.vigener.file.pathprovider.InputFilePathProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,19 +15,31 @@ public class FileManager {
 
     private final VigenerAlgorithm vigenerAlgorithm;
     private final FileWork fileWork;
-    private final FilePath filePath;
     private final FileKey fileKey;
 
+
+    private final AnalyzedFilePathProvider analyzedFilePath;
+    private final EncryptedFilePathProvider encryptedFilePath;
+    private final DecryptedFilePathProvider decryptedFilePath;
+    private final InputFilePathProvider inputFilePath;
+
     public void encrypt() {
-        String readFile = fileWork.readFile(filePath.inputFilePath());
+        String readFile = fileWork.readFile(inputFilePath.getPath());
         String encrypted = vigenerAlgorithm.encrypt(readFile, fileKey.key());
-        fileWork.saveFile(encrypted, filePath.encryptedFilePath());
+        fileWork.saveFile(encrypted, encryptedFilePath.getPath());
     }
 
     public void decrypt() {
-        String readFile = fileWork.readFile(filePath.encryptedFilePath());
+        String readFile = fileWork.readFile(encryptedFilePath.getPath());
         String decrypted = vigenerAlgorithm.decrypt(readFile, fileKey.key());
-        fileWork.saveFile(decrypted, filePath.decryptedFilePath());
+        fileWork.saveFile(decrypted, decryptedFilePath.getPath());
+    }
+
+    public void analyze(){
+        String readFile = fileWork.readFile(encryptedFilePath.getPath());
+        FrequencyAnalysis analysis = new FrequencyAnalysis(readFile);
+        String analyzed = analysis.analyze();
+        fileWork.saveFile(analyzed, analyzedFilePath.getPath());
     }
 
 }
